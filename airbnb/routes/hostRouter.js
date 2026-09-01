4b8f402f7c5c3c09c3db4670ee0ec3de4931ac66
Raj Shekhar Patel <patelrajshekhar6@gmail.com>
2026-09-01 19:53:43 +0530

    applying concepts

---
+++ airbnb/routes/hostRouter.js
@@ -0,0 +1,18 @@
+//Core module
+const path = require('path');
+
+//External Module
+const express = require('express');
+
+const hostRouter = express.Router();
+
+
+hostRouter.get( "/add-home", (req, res, next) =>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'addHome.html'));
+  })
+
+  hostRouter.post( "/add-home", (req, res, next) =>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'homeAdded.html'));
+  })
+
+module.exports = hostRouter;
